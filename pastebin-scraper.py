090debb114ee6280da6ded01f39c3f80acf7041a
Dominic Jomaa <dominic.jomaa@gmail.com>
2018-05-30 18:21:35 +0100

    Updated file saving, header appending, moved code around

--- pastebin-scraper.py
+++ pastebin-scraper.py
@@ -75,79 +75,112 @@ def fetchPaste(addr,link):
     paste = tree.xpath('//*[@id="paste_code"]/text()')
 
     # validating return and returning
-    if(len(paste) == 1):
-        Log(4,"Scrape ["+addr+link+"]...")
-        return str(paste[0]).encode("utf-8")
-    else :
+    if(len(paste) == 0):
         Log(2,"Scrape ["+addr+link+"]...")
         return str(addr+link)
 
-def makeHeader(addr):
+    # logging successful scrape
+    Log(4,"Scrape ["+addr+link+"]...")
+
+    # fetching title of paste
+    title = tree.xpath('//*[@id="content_left"]/div[2]/div[3]/div[1]/h1/text()')[0]
+
+    # fetching type of paste
+    pasteType = tree.xpath('//*[@id="code_buttons"]/span[2]/a/text()')[0]
+
+    # fetching date of paste
+    date = tree.xpath('//*[@id="content_left"]/div[2]/div[3]/div[2]/span/text()')[0]
+
+    # saving new header and newly scraped content to composite file
+    Save(filename,makeHeader(title,date,pasteType,addr+link,link))
+    Save(filename,str(paste[0]).encode("utf-8"))
+
+    # returning content string
+    return str(paste[0]).encode("utf-8")
+
+# building text header for scraped content
+def makeHeader(name,date,pasteType,addr,id):
     header = "\n"
     header += "-"*20
     header += "\n"
-    header += "Title   : Performing Request\n"
-    header += "Time    : "+Now()+"\n"
-    header += "Address : "+addr+"\n"
+    header += "Title      : "+name+"\n"
+    header += "Date       : "+date+"\n"
+    header += "Fetch Time : "+Now()+"\n"
+    header += "Address    : "+addr+"\n"
+    header += "ID         : "+id+"\n"
+    header += "Type       : "+pasteType+"\n"
     return header
 
 # method to perform a structued log
 def Log(type,msg):
+    # type checking for output and coloration
+    if  (type == 1):
+        typeStr = "LOG"
+        typeCol = bcolors.OKBLUE
+    elif(type == 2):
+        typeStr = "ERROR"
+        typeCol = bcolors.FAIL
+    elif(type == 3):
+        typeStr = "WARNING"
+        typeCol = bcolors.WARNING
+    elif(type == 4):
+        typeStr = "SUCCESS"
+        typeCol = bcolors.OKGREEN
+    else :
+        typeStr = "LOG"
+        typeCol = bcolors.OKBLUE
 
-    if  (type == 1): typeStr = bcolors.OKBLUE  + "LOG"     + bcolors.ENDC
-    elif(type == 2): typeStr = bcolors.FAIL    + "ERROR"   + bcolors.ENDC
-    elif(type == 3): typeStr = bcolors.WARNING + "WARNING" + bcolors.ENDC
-    elif(type == 4): typeStr = bcolors.OKGREEN + "SUCCESS" + bcolors.ENDC
-    else :           typeStr = bcolors.OKBLUE  + "LOG"     + bcolors.ENDC
+    # printing lof to terminal
+    print("[\033[92m %s \033[0m]  %s%-7s%s : %s" % (Now(),typeCol,typeStr,bcolors.ENDC,msg))
 
-    print("[\033[92m %s \033[0m] : %-7s : %s" % (Now(),typeStr,msg))
-
-
-def Save(filename,content):
-    with open(filename, 'a') as f:
+# appending content to file
+def Save(fname,content):
+    with open(fname, 'a') as f:
         f.write(str(content))
 
+# Timenow string
 def Now():
     return datetime.datetime.now().strftime('%d-%m-%Y:%H-%M-%S')
 
+# Time now string filename safe
 def NowString():
     return datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
 
+# thread sleep function including sleep
 def Sleep(time,msg=None):
     if(msg) : Log(1,msg)
     else    : Log(1,bcolors.OKGREEN+"Sleeping : "+str(time)+"s"+ bcolors.ENDC)
     sleep(time)
 
-def init():
+# file name for save data
+filename = "Scrape-"+NowString()+".txt"
 
-    # file name for save data
-    filename = "Scrape-"+NowString()+".txt"
+def init():
 
     Save(filename,"Starting Scrape : "+Now()+"\n")
 
     # list of archive page links
     archivelinks = fetchLinks(addr)
 
-    if(len(archivelinks) == 0): Save(filename,"Scrape Failed : API Limit Hit ")
+    # checking pastebin list of id's
+    if(len(archivelinks) == 0):
+        Save(filename,"Scrape Failed : API Limit Hit ")
+        return
 
+    # appending id's individually to file as list
     for i in range(len(archivelinks)):
         Save(filename," %-3s : id : %s \n" % (str(i),archivelinks[i]))
 
     # list of scraped text
     scrapes = []
 
-    if(len(archivelinks) > 0):
-        for i in archivelinks:
-
-            Sleep(2+random.uniform(0.1,0.5))
-
-            Save(filename,makeHeader(addr+i))
-
-            scrape = fetchPaste(addr,i)
-
-            Save(filename,scrape)
-
-            scrapes.append(scrape);
-
+    # iterating over pastebin links
+    for i in archivelinks:
+        # sleeping before attempting next fetch
+        Sleep(2+random.uniform(0.1,0.5))
+        # attempting sub fetch on scraped id
+        scrape = fetchPaste(addr,i)
+        # storing scrape content inside array
+        scrapes.append(scrape);
 
 init()
